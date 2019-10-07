deb61dce6744e4dd0a3d83f75810758a41f0922a
hdinhof1 <hdinhof1@binghamton.edu>
2019-10-07 13:17:39 -0400

    tweaking yaml file for variable

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
-filesToCheck='realm-update-reminder.yml|learning.swift' # just go by filename or part of path
-filesModified=$(git diff "$GIT_DESTINATION_BRANCHNAME" "$GIT_PR_BRANCHNAME" --unified=0 --name-only | egrep $filesToCheck)
-if [ ! -z "$search" ]
-then 
+echo "Beginning work"
+filesToCheck="learning.swift"
+filesModified=$(git diff master test-branch5 --unified=0 --name-only | egrep $filesToCheck)
+echo "filesModified is $filesModified" 
+if [ ! -z "$filesModified" ]; then 
 	echo "Modified a Realm file, continue to check for version bump"
 else
+	echo "No learning.swift is modified exiting"
 	exit 0
 fi
 search=$(git diff 31c6b3f0f2c46ff006eece209849e0bed1d7c4a8 --unified=0 -G'Realm.configureRealm\(schemaVersion:' learning.swift | egrep '[+-]Realm.configureRealm\(schemaVersion:')
@@ -24,3 +26,6 @@ echo Done!
 # then do echo $search | grep + | egrep -o [0-9]+  # to get the number of +
 # the base of the merge commit (on master) is 4ce19158ecac5ddeff87e87ee406fce820f823a2 
 # we want the most recent version of master
+
+
+          # just go by filename or part of path
