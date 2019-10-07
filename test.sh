a3ba5b14bc88817246dec3742f63833253d738a8
hdinhof1 <hdinhof1@binghamton.edu>
2019-10-07 18:29:28 -0400

    adds failure double pipe || false

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 echo "Beginning work"
 filesToCheck="learning.swift"
-filesModified=$(git diff master test-branch5 --name-only | egrep 'learning.swift')
+filesModified=$(git diff master test-branch5 --name-only | egrep 'realm' || false)
 echo "filesModified is $filesModified" 
 if [ ! -z "$filesModified" ]
 then 
