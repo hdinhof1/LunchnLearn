7a5121970d4eec33a375f2376f2f6b33f916038a
hdinhof1 <hdinhof1@binghamton.edu>
2019-10-07 17:55:12 -0400

    try double brackets for if clause

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 echo "Beginning work"
 filesToCheck="learning.swift"
-filesModified=$(git diff master test-branch5 --unified=0 --name-only | egrep "$filesToCheck")
+filesModified=$(git diff master test-branch5 --name-only | egrep "$filesToCheck")
 echo "filesModified is $filesModified" 
 if [ ! -z "$filesModified" ]
 then 
 	echo "Modified a Realm file, continue to check for version bump"
 else
-	echo "No learning.swift is modified exiting"
+	echo "No $filesToCheck is modified exiting"
 	exit 0
 fi
 search=$(git diff 31c6b3f0f2c46ff006eece209849e0bed1d7c4a8 --unified=0 -G'Realm.configureRealm\(schemaVersion:' learning.swift | egrep '[+-]Realm.configureRealm\(schemaVersion:')
