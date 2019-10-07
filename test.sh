17f2d1f78ad20cd492f0cc2332fffb7884f2a717
hdinhof1 <hdinhof1@binghamton.edu>
2019-10-07 13:00:16 -0400

    fix variable yaml

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
+filesToCheck='realm-update-reminder.yml|learning.swift' # .yml here to remind that the path is based on the location of the file
+filesModified=$(git diff "$GIT_DESTINATION_BRANCHNAME" "$GIT_PR_BRANCHNAME" --unified=0 --name-only | egrep $filesToCheck)
+if [ ! -z "$filesModified" ]
+then 
+	echo "Modified a Realm file, continue to check for version bump"
+else
+	exit 0
+fi
 search=$(git diff 31c6b3f0f2c46ff006eece209849e0bed1d7c4a8 --unified=0 -G'Realm.configureRealm\(schemaVersion:' learning.swift | egrep '[+-]Realm.configureRealm\(schemaVersion:')
 before=$(echo $search | egrep -o [0-9]+ | head -1)
 after=$(echo $search | egrep -o [0-9]+ | tail -1 )
