0a7f78afe0d41c1cce7cfb5084c669d23b30cbfc
hdinhof1 <hdinhof1@binghamton.edu>
2019-10-08 16:45:39 -0400

    fix back success version and print statement

--- learning.swift
+++ learning.swift
@@ -23,4 +23,4 @@ print("modify line git diff learning.swift")
 
 print("lets do a changed file in a pr")
 print("at work more stuff here")
-Realm.configureRealm(schemaVersion: 42)
+Realm.configureRealm(schemaVersion: 48)
