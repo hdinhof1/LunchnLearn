3308ef8b31b22f0c73054fe71ce371784371ba43
hdinhof1 <hdinhof1@binghamton.edu>
2019-10-08 14:02:10 -0400

    modifies the testcase .swift file should triggerexit 10

--- learning.swift
+++ learning.swift
@@ -11,6 +11,10 @@ print("added")
 
 print("Hi")
 print("everyone")
+print("Adding a whole bunch of text here")
+print("Adding a whole bunch of text above the area that should be with you know what")
+print("Adding a whole bunch of text here away from the patch area that may overlap with")
+print("any text below")
 
 print("changing this should trigger the realm update scan")
 print("switching yml to checkout@v1")
