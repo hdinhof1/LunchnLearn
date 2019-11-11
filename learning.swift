42c69b49748d6da5a9faf376df856101550728e2
hdinhof1 <hdinhof1@binghamton.edu>
2019-11-11 15:20:55 -0500

    sets back learning.swift to be unchanged

--- learning.swift
+++ learning.swift
@@ -11,10 +11,6 @@ print("added")
 
 print("Hi")
 print("everyone")
-print("Adding a whole bunch of text here")
-print("Adding a whole bunch of text above the area that should be with you know what")
-print("Adding a whole bunch of text here away from the patch area that may overlap with")
-print("any text below")
 
 print("changing this should trigger the realm update scan")
 print("switching yml to checkout@v1")
@@ -23,4 +19,4 @@ print("modify line git diff learning.swift")
 
 print("lets do a changed file in a pr")
 print("at work more stuff here")
-Realm.configureRealm(schemaVersion: 48)
+Realm.configureRealm(schemaVersion: 47)
